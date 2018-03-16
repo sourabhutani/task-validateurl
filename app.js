ae15a4ddf6c5dc7848b9ed37e69b7ea675dfabb2
sourabhutani <sourabh.bhutani@kelltontech.com>
2018-03-16 18:52:08 +0530

    added routes and controller data

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var express = require('express');
 // var app = express();
 var body_parser = require('body-parser');
-
+var app = module.exports=express();
 
 // mongoose.connect('mongodb://localhost:27017/ServerTokenDemo');
-
-var app=module.exports=express();
+// custom modules 
+routes=require('./routes/routes');
 
 app.set('env',process.env.NODE_ENV || 'production');
-
 app.use(body_parser.urlencoded({ extended : true}));
-
 app.use(body_parser.json());
 
-// routes=require('./routes/routes');
-// app.use('/api',routes);
+app.use('/api',routes);
 
 
 var port = process.env.PORT || 3000;
---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+var express= require('express');
+var router=express.Router();
+var ValidateUrlController=require('../controllers/validateurlController');
+
+router.route('/v1/validate/:uri-data')
+.get(ValidateUrlController.validateUri);
+
+module.exports=router;
