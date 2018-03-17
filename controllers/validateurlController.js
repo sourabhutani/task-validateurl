ffc2ac736b1ceaa58504529ef3238731626a3824
sourabhutani <sourabhutani@gmail.com>
2018-03-17 23:44:38 +0530

    added file

--- controllers/validateurlController.js
+++ controllers/validateurlController.js
@@ -3,14 +3,21 @@ var app=express();
 var request = require('request');
 
 exports.validateUri = (req,res) => {
-var url = req.params.uri;
-
-request(url, function (error, response, body) {
-  if(error){
-    res.json({'error' : 'Invalid url' });
-  }
-  console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-
-});
-
+    // Setting URL and headers for request
+    var url = req.params.uri;
+    // Return new promise 
+    return new Promise((resolve, reject) => {
+     // Do async job
+        request(url, (error, response, body) => {
+           // console.log(url);
+            if (error) {
+                res.json({'error' : 'Invalid url' });
+               // reject(err);
+            }
+            if (response) {
+                res.json({'success' : 'url is working fine' });
+                // resolve(JSON.parse(body));
+            }
+        })
+    });
 }
